fbdea51dcc4124665d6b39c589127637ce5b4f32
Maria Eduarda F. Teixeira <138616948+Maria-Eduarda-FT@users.noreply.github.com>
2025-03-07 20:30:29 -0300

    Update Apple.java

--- celulares/Apple.java
+++ celulares/Apple.java
@@ -1,9 +1,18 @@
-public class Apple implements FabricanteCelular{
-    
-    @Override public Celular constroiCelular (String modelo){
-        if (modelo == "iphoneX"){
-            return new iphoneX();
-        } 
-        returnnull;
+public class Apple implements FabricanteCelular {
+
+    private static Apple instancia;
+
+    private Apple() {}
+
+    public static Apple getInstancia() {
+        if (instancia == null) {
+            instancia = new Apple();
+        }
+        return instancia;
+    }
+
+    @Override
+    public Celular constroiCelular(String modelo) {
+        return new iphoneX(modelo);
     }
 }
