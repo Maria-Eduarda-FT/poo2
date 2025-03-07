bda12a4e0970d6e5e0b40a248a145763a486494c
Maria Eduarda F. Teixeira <138616948+Maria-Eduarda-FT@users.noreply.github.com>
2025-03-07 20:31:24 -0300

    Update iphoneX.java

--- celulares/iphoneX.java
+++ celulares/iphoneX.java
@@ -1,5 +1,18 @@
-public class iphoneX implements Celular{
-    
-    @Override public void fazLigacao(){}
-    @Override public void tiraFoto(){}
+public class iphoneX implements Celular {
+
+    private String modelo;
+
+    public iphoneX(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public void fazLigacao() {
+        System.out.println("Fazendo ligação com o iPhone " + modelo);
+    }
+
+    @Override
+    public void tiraFoto() {
+        System.out.println("Tirando foto com a câmera do iPhone " + modelo);
+    }
 }
